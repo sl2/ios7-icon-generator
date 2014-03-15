e9735613ef7921fa73d43e08738ca5e6e6b6495b
sl2 <dimension.sf@gmail.com>
2014-03-15 19:08:41 +0900

    fix #4

--- ios7-icon-generator.sh
+++ ios7-icon-generator.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 mkdir ./icons
-convert $1 -geometry 120x120 ./icons/Icon-60@2x.png
-convert $1 -geometry 58x58 ./icons/Icon-Small@2x.png
-convert $1 -geometry 80x80 ./icons/Icon-40@2x.png
-convert $1 -geometry 512x512 ./icons/iTunesArtwork.png
-convert $1 -geometry 1024x1024 ./icons/iTunesArtwork@2x.png
+convert $1 -geometry 120x120 ./icons/Icon-App.png
+convert $1 -geometry 58x58 ./icons/Icon-Settings.png
+convert $1 -geometry 80x80 ./icons/Icon-Spotlight.png
+convert $1 -geometry 512x512 ./icons/iPhonePortrait@2x.png
+convert $1 -geometry 1024x1024 ./icons/iPhonePortrait@R4.png
 
 
